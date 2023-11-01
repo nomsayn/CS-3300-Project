dba9a53cc2b9fda3a1391594aa2c7a31559750a3
nomsayn <mmichela11@gmail.com>
2023-10-31 18:00:31 -0600

    Restart Project for Game Library App

--- AssetTracker/AssetsAndPortfolioApp/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import WebSiteUser
-
-class UserForm(ModelForm):
-    class Meta:
-        model = WebSiteUser
-        fields = ['name', 'email', 'platform']
--- GameLibraryProject/game_library_app/apps.py
+++ GameLibraryProject/game_library_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AssetsandportfolioappConfig(AppConfig):
+class GameLibraryAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AssetsAndPortfolioApp'
+    name = 'game_library_app'
